f06d84d07477a47f0a6ca3c54c8464614e238934
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-08-31 11:31:52 +0530

    feat: add boilerplate clap and anyhow code

---
+++ src/cli.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+
+use clap::{Parser, ValueEnum};
+
+// Simple find utility written in Rust.
+#[derive(Debug, Parser)]
+#[command(name = "riff", version, about)]
+pub struct Args {
+    #[arg(default_value = ".")]
+    pub root: PathBuf,
+
+    #[arg(short = 'n', long)]
+    pub name: Option<String>,
+
+    #[arg(short = 't', long = "type", value_enum)]
+    pub kind: Option<Kind>,
+
+    #[arg(short = 'd', long)]
+    pub max_depth: Option<usize>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, ValueEnum)]
+pub enum Kind {
+    #[value(alias = "f")]
+    File,
+    #[value(alias = "d")]
+    Dir,
+    #[value(alias = "l")]
+    Symlink,
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod cli;
+pub mod matcher;
+pub mod traversal;
