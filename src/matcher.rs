a7a81c360c8cf23deb2c73612d901d73e21ec805
Saksham Anand <saksham261261cp@gmail.com>
2026-09-01 12:29:52 +0530

    feat: add basic matching

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,8 +1,35 @@
-#![allow(unused_variables)]
-
-use crate::cli::Args;
+use crate::cli::{Args, Kind};
 use std::{fs::Metadata, path::Path};
 
 pub fn matches(args: &Args, path: &Path, meta: &Metadata) -> bool {
-    todo!()
+    if let Some(kind) = &args.kind {
+        match kind {
+            Kind::File => {
+                if !meta.is_file() {
+                    return false;
+                }
+            }
+            Kind::Dir => {
+                if !meta.is_dir() {
+                    return false;
+                }
+            }
+            Kind::Symlink => {
+                if !meta.is_symlink() {
+                    return false;
+                }
+            }
+        }
+    }
+
+    if let Some(filename) = path.file_name() {
+        if let Some(name) = &args.name {
+            if name.as_str() != filename {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
+    true
 }
